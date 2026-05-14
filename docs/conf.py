a4de5aaafe6e9d996c606313330eba79e0afa41f
Justin B. Kinney <jkinney@cshl.edu>
2026-05-14 07:15:46 -0400

    Add docs sidebar logo

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ master_doc = "index"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "**.ipynb_checkpoints"]
+exclude_patterns = ["_build", "build", "html", "**.ipynb_checkpoints"]
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -76,7 +76,17 @@ exclude_patterns = ["_build", "**.ipynb_checkpoints"]
 #
 html_theme = "sphinx_rtd_theme"
 
+html_logo = "_static/gaugefixer_logo.png"
+
+html_theme_options = {
+    "style_nav_header_background": "#ffffff",
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["figures"]
+html_static_path = ["_static"]
+
+html_css_files = [
+    "custom.css",
+]
