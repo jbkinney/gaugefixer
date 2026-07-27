issues with the indexing of the background-averaged epistasis basis fixed

gaugefixer/models.py
@@ -300,7 +300,7 @@ class HierarchicalModel(object):
             used in the `background-average` epistasis basis.
         """
         position_basis = get_position_basis(
-            encoding=basis_name,
+            basis_name=basis_name,
             wt_seq=wt_seq,
             pi_lc=pi_lc,
             position_basis=None,
@@ -310,7 +310,7 @@ class HierarchicalModel(object):
             np.vstack([np.zeros((1, alpha)), np.eye(alpha)])
             for alpha in self.alphas
         ]
-        pi_lc, lda = self.fixer._get_pi_lc_lda(gauge="zero-sum") # type: ignore
+        pi_lc, lda = self.fixer._get_pi_lc_lda(gauge="zero-sum")  # type: ignore
         Ps2 = self.fixer._get_site_P(pi_lc, lda)
         Ps = [P2 @ P1 @ B for P1, P2, B in zip(Ps1, Ps2, position_basis)]
 
@@ -326,7 +326,7 @@ class HierarchicalModel(object):
         project = ParameterProjector(
             generating_orbits_param_idx, Ps=Ps, use_dense_matrix=False
         )
-        
+
         coeffs_names = get_orbits_features(
             self.orbits, self.alphabet_list, wt_seq=wt_seq
         )
@@ -361,7 +361,7 @@ class HierarchicalModel(object):
             Coefficients associated to the basis vectors.
         """
         position_basis = get_position_basis(
-            encoding=basis_name,
+            basis_name=basis_name,
             wt_seq=wt_seq,
             pi_lc=pi_lc,
             position_basis=None,
@@ -372,7 +372,7 @@ class HierarchicalModel(object):
             for alpha in self.alphas
         ]
         Ps = [np.linalg.solve(B, X) for X, B in zip(Xs, position_basis)]
-        
+
         idx = get_generating_orbits_param_idx(
             self.generating_orbits,
             self.alphabet_list,

gaugefixer/utils.py
@@ -565,7 +565,9 @@ def get_orbits_features(
                     ]
                     for i in orbit
                 ]
-                features.extend([(orbit, s) for s in get_all_seqs(alphabets, sep=';')])
+                features.extend(
+                    [(orbit, s) for s in get_all_seqs(alphabets, sep=";")]
+                )
     return features
 
 
@@ -686,7 +688,7 @@ def get_generating_orbits_param_idx(
     return generating_orbits_param_idx
 
 
-def get_all_seqs(alphabet_list, seqs=[""], sep=''):
+def get_all_seqs(alphabet_list, seqs=[""], sep=""):
     if len(alphabet_list) == 0:
         return seqs
     elif len(alphabet_list) > 100:
@@ -702,7 +704,7 @@ def get_all_seqs(alphabet_list, seqs=[""], sep=''):
 
 
 def get_position_basis(
-    encoding: str | None = None,
+    basis_name: str | None = None,
     wt_seq: str | None = None,
     pi_lc: list[np.ndarray] | None = None,
     position_basis: list[np.ndarray] | None = None,
@@ -711,10 +713,32 @@ def get_position_basis(
     """
     Get the position basis for the given encoding and wild-type sequence
     to be used as reference or the provided `position_basis`.
+
+    Parameters
+    ----------
+    basis_name : str
+        Name of the basis to use from `background-averaged` and `fourier`.
+    wt_seq : str
+        Wild-type sequence to use for basis conversion.
+    pi_lc : list of np.ndarray or None, optional
+        Probability of each character at each position
+        used in the `background-average` epistasis basis.
+    position_basis : list of np.ndarray, optional
+        List of position-specific basis matrices. Must be provided if `basis_name` is None.
+    alphabet_list : list of list of str, optional
+        List of alphabets, where each alphabet is a list of characters to sample
+        from for that specific position. The length of ``alphabet_list`` determines
+        the sequence length.
+
+    Returns
+    -------
+    list of np.ndarray
+        List of position-specific basis matrices. Each matrix has shape (alpha, alpha),
+        where alpha is the number of characters in the corresponding site-specific alphabet.
     """
     # Check input validity
     if (
-        encoding is not None
+        basis_name is not None
         and wt_seq is not None
         and position_basis is None
         and alphabet_list is not None
@@ -729,7 +753,7 @@ def get_position_basis(
                     f"Character '{c}' in wt_seq is not in the corresponding alphabet {alphabet}"
                 )
 
-        if encoding == "background-averaged":
+        if basis_name == "background-averaged":
             # Check validity of pi_lc
             if pi_lc is None:
                 pi_lc = [
@@ -756,18 +780,14 @@ def get_position_basis(
             for c, alphabet, pi_l in zip(wt_seq, alphabet_list, pi_lc):
                 alpha = len(alphabet)
                 i = alphabet.index(c)
-                basis_inv = np.zeros((alpha + 1, alpha + 1))
-                basis_inv[0] = 1.0 / alpha
-                basis_inv[1:, 1:] = np.eye(alpha)
-                basis_inv[1:, i + 1] -= 1
-                idx = np.arange(alpha + 1) != (i + 1)
-                basis_inv = basis_inv[idx, 1:]
-                basis_inv[0] = pi_l
+                basis_inv = np.eye(alpha)
+                basis_inv[:, i] -= 1
+                basis_inv[i] = pi_l
 
                 basis = np.linalg.inv(basis_inv)
                 position_basis.append(basis)
 
-        elif encoding == "fourier":  # from Brookes et al. 2022
+        elif basis_name == "fourier":  # from Brookes et al. 2022
             position_basis = []
 
             if pi_lc is not None:
@@ -787,10 +807,10 @@ def get_position_basis(
                 position_basis.append(basis)
         else:
             raise ValueError(
-                f"Invalid encoding '{encoding}'; must be 'background-averaged' or 'fourier'"
+                f"Invalid basis_name '{basis_name}'; must be 'background-averaged' or 'fourier'"
             )
     elif (
-        encoding is None
+        basis_name is None
         and wt_seq is None
         and pi_lc is None
         and position_basis is not None
@@ -811,6 +831,6 @@ def get_position_basis(
                 )
     else:
         raise ValueError(
-            "Invalid combination of inputs: must provide either (encoding, wt_seq, alphabet_list) or (position_basis, alphabet_list)"
+            "Invalid combination of inputs: must provide either (basis_name, wt_seq, alphabet_list) or (position_basis, alphabet_list)"
         )
     return position_basis

test/test_models.py
@@ -74,7 +74,6 @@ class TestModels(unittest.TestCase):
             positions=positions,
             generating_orbits=orbits,
         )
-        print(model.features)
         self.assertGreater(model.n_orbits, len(orbits))
         self.assertTrue(set(orbits).issubset(set(model.get_orbits())))
 
@@ -323,9 +322,7 @@ class TestModels(unittest.TestCase):
 
         with self.assertRaises(KeyError):
             theta = np.random.normal(size=model.n_features)
-            theta = pd.Series(
-                theta, index=model.features[:-1] + [((-1,), "A")]
-            )
+            theta = pd.Series(theta, index=model.features[:-1] + [((-1,), "A")])
             model.set_params(theta)
 
         with self.assertRaises(ValueError):
@@ -650,15 +647,13 @@ class TestBasisChange(unittest.TestCase):
             coeffs1, basis_name="background-averaged", wt_seq=wt_seq
         )
         f2 = model(seqs)
-        
-        print(f1, f2)
+
         assert np.allclose(f1, f2)
 
         coeffs2 = model.get_basis_coeffs(
             basis_name="background-averaged", wt_seq=wt_seq
         )
         assert np.allclose(coeffs1, coeffs2)
-        
 
     def test_get_background_averaged_coeffs_additive_model_multiallelic(self):
         wt_seq = "TCG"
@@ -736,36 +731,52 @@ class TestBasisChange(unittest.TestCase):
         )
 
     def test_get_set_basis_coeffs(self):
-        L = 3
+        L = 4
         models = [
-            # AllOrderModel(alphabet_name="dna", L=L),
+            AllOrderModel(alphabet_name="dna", L=L),
             AdditiveModel(alphabet_name="dna", L=L),
             PairwiseModel(alphabet_name="dna", L=L),
             KorderModel(alphabet_name="dna", L=L, K=2),
             NeighborModel(alphabet_name="dna", L=L),
             KadjacentModel(alphabet_name="dna", L=L, K=2),
         ]
+        seqs = ["".join(x) for x in product(list("ACGT"), repeat=L)]
 
-        wt_seqs = [
+        wt_seqs1 = [
+            "".join(x)
+            for x in np.random.choice(models[0].alphabet, size=(5, L))
+        ]
+        wt_seqs2 = [
             "".join(x)
             for x in np.random.choice(models[0].alphabet, size=(5, L))
         ]
         for model in models:
             for basis_name in ["background-averaged", "fourier"]:
-                for wt_seq in wt_seqs:
+                for wt_seq1, wt_seq2 in zip(wt_seqs1, wt_seqs2):
                     model.set_random_params()
+                    f1 = model(seqs)
                     coeffs1 = model.get_basis_coeffs(
-                        basis_name=basis_name, wt_seq=wt_seq
+                        basis_name=basis_name, wt_seq=wt_seq1
                     )
                     model.set_basis_coeffs(
-                        coeffs1, basis_name=basis_name, wt_seq=wt_seq
+                        coeffs1, basis_name=basis_name, wt_seq=wt_seq1
                     )
+                    f2 = model(seqs)
+                    assert np.allclose(f1, f2)
+
                     coeffs2 = model.get_basis_coeffs(
-                        basis_name=basis_name, wt_seq=wt_seq
+                        basis_name=basis_name, wt_seq=wt_seq1
                     )
-                    print(wt_seq, basis_name)
-                    print(coeffs1, coeffs2)
                     assert np.allclose(coeffs1, coeffs2)
+
+                    coeffs3 = model.get_basis_coeffs(
+                        basis_name=basis_name, wt_seq=wt_seq2
+                    )
+                    model.set_basis_coeffs(
+                        coeffs3, basis_name=basis_name, wt_seq=wt_seq2
+                    )
+                    f2 = model(seqs)
+                    assert np.allclose(f1, f2)
 
 
 if __name__ == "__main__":

test/test_utils.py
@@ -1067,6 +1067,7 @@ class TestNamedAlphabetsDict(unittest.TestCase):
 
 class TestSubsequences(unittest.TestCase):
     """Test utilities for subsequence generation."""
+
     def test_get_orbits_subsequences(self):
         orbits = [(0, 1)]
         alphabet = ["A", "B", "C"]
@@ -1081,19 +1082,23 @@ class TestSubsequences(unittest.TestCase):
         )
         exp = ["BB*", "BC*", "CB*", "CC*"]
         assert subsequences == exp
-    
+
     def test_get_orbits_subsequences_wt_binary(self):
         orbits = [(0, 1)]
         alphabet = ["A", "B"]
         alphabet_list = [alphabet] * 3
-        subsequences = get_orbits_subsequences(orbits, alphabet_list, wt_seq="AAA")
+        subsequences = get_orbits_subsequences(
+            orbits, alphabet_list, wt_seq="AAA"
+        )
         exp = ["BB*"]
         assert subsequences == exp
-        
+
         orbits = [(0, 1)]
         alphabet = ["A", "B"]
         alphabet_list = [alphabet] * 3
-        subsequences = get_orbits_subsequences(orbits, alphabet_list, wt_seq="AAA")
+        subsequences = get_orbits_subsequences(
+            orbits, alphabet_list, wt_seq="AAA"
+        )
         exp = ["BB*"]
         assert subsequences == exp
 
@@ -1128,28 +1133,28 @@ class TestBasis(unittest.TestCase):
     def test_get_background_averaged_basis_invalid_inputs(self):
         alphabet = ["A", "B", "C"]
         configs = [
-            {"encoding": "wild-type"},
-            {"encoding": "background-averaged", "wt_seq": "A"},
-            {"encoding": "background-averaged", "alphabet_list": "A"},
+            {"basis_name": "wild-type"},
+            {"basis_name": "background-averaged", "wt_seq": "A"},
+            {"basis_name": "background-averaged", "alphabet_list": "A"},
             {
-                "encoding": "background-averaged",
+                "basis_name": "background-averaged",
                 "wt_seq": "A",
                 "alphabet_list": [alphabet] * 2,
             },
             {
-                "encoding": "background-averaged",
+                "basis_name": "background-averaged",
                 "wt_seq": "",
                 "alphabet_list": [alphabet],
             },
-            {"encoding": "fourier", "wt_seq": "A"},
+            {"basis_name": "fourier", "wt_seq": "A"},
             {
-                "encoding": "fourier",
+                "basis_name": "fourier",
                 "wt_seq": "A",
                 "alphabet_list": [alphabet],
                 "pi_lc": [np.array([0, 1, 0.0])],
             },
             {
-                "encoding": "fourier",
+                "basis_name": "fourier",
                 "wt_seq": "AA",
                 "alphabet_list": [alphabet],
             },
@@ -1162,11 +1167,10 @@ class TestBasis(unittest.TestCase):
     def test_get_background_averaged_basis(self):
         alphabet_list = [["A", "B", "C"]]
         basis = get_position_basis(
-            encoding="background-averaged",
+            basis_name="background-averaged",
             wt_seq="A",
             alphabet_list=alphabet_list,
         )[0]
-        print(basis)
         assert np.allclose(
             basis,
             [
@@ -1179,32 +1183,48 @@ class TestBasis(unittest.TestCase):
         f = np.array([2, 0, 1])
         coeffs = np.linalg.solve(basis, f)
         assert np.allclose(coeffs, [1, -2, -1])
-        
+
         # Alternative reference
         basis = get_position_basis(
-                    encoding="background-averaged",
-                    wt_seq="B",
-                    alphabet_list=alphabet_list,
-                )[0]
-        print(basis)
+            basis_name="background-averaged",
+            wt_seq="B",
+            alphabet_list=alphabet_list,
+        )[0]
         assert np.allclose(
             basis,
             [
-                [1, 2 / 3.0, -1 / 3.0],
-                [1, -1 / 3.0, -1 / 3.0],
-                [1, -1 / 3.0, 2 / 3.0],
+                [2 / 3.0, 1, -1 / 3.0],
+                [-1 / 3.0, 1, -1 / 3.0],
+                [-1 / 3.0, 1, 2 / 3.0],
             ],
         )
 
         f = np.array([2, 0, 1])
         coeffs = np.linalg.solve(basis, f)
-        assert np.allclose(coeffs, [1, -2, -1])
-        
+        assert np.allclose(coeffs, [2, 1, 1])
+
+        basis = get_position_basis(
+            basis_name="background-averaged",
+            wt_seq="C",
+            alphabet_list=alphabet_list,
+        )[0]
+        assert np.allclose(
+            basis,
+            [
+                [2 / 3.0, -1 / 3.0, 1],
+                [-1 / 3.0, 2 / 3.0, 1],
+                [-1 / 3.0, -1 / 3.0, 1],
+            ],
+        )
+
+        f = np.array([2, 0, 1])
+        coeffs = np.linalg.solve(basis, f)
+        assert np.allclose(coeffs, [1, -1, 1])
 
     def test_get_pi_background_averaged_basis(self):
         alphabet_list = [["A", "B", "C"]]
         basis = get_position_basis(
-            encoding="background-averaged",
+            basis_name="background-averaged",
             wt_seq="A",
             pi_lc=[np.array([0.5, 0.3, 0.2])],
             alphabet_list=alphabet_list,
@@ -1220,7 +1240,7 @@ class TestBasis(unittest.TestCase):
     def test_get_fourier_basis(self):
         alphabet_list = [["A", "B", "C"]]
         basis = get_position_basis(
-            encoding="fourier",
+            basis_name="fourier",
             wt_seq="A",
             alphabet_list=alphabet_list,
         )[0]
@@ -1243,7 +1263,7 @@ class TestBasis(unittest.TestCase):
         coeffs = np.linalg.solve(basis, f)
         assert np.allclose(coeffs, basis @ f)
         assert np.allclose(f, basis @ coeffs)
-        
+
 
 if __name__ == "__main__":
     import sys